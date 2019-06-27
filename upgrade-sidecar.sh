68da7b051bd64b482878b6a6a9a20417bcb22255
Nguyen Hai Truong <truongnh@vn.fujitsu.com>
2019-06-27 15:32:01 +0700

    Create upgrade-sidecar.sh

---
+++ upgrade-sidecar.sh
@@ -0,0 +1,31 @@
+# based on the "patch deployment" strategy in this comment:
+# https://github.com/kubernetes/kubernetes/issues/13488#issuecomment-372532659
+# requires jq
+
+# $1 is a valid namespace
+
+if [ $# -ne 1 ]; then
+    echo $0": usage: ./refresh.sh <namespace>"
+    exit 1
+fi
+
+NS=$1
+
+function refresh-all-pods() {
+    echo
+    DEPLOYMENT_LIST=$(sudo kubectl -n $NS get deployment -o jsonpath='{.items[*].metadata.name}')
+    echo "Refreshing pods in all Deployments"
+    for deployment_name in $DEPLOYMENT_LIST ; do
+        TERMINATION_GRACE_PERIOD_SECONDS=$(sudo kubectl -n $NS get deployment "$deployment_name" -o jsonpath='{.spec.template.spec.terminationGracePeriodSeconds}')
+    if [ "$TERMINATION_GRACE_PERIOD_SECONDS" -eq 30 ]; then
+        TERMINATION_GRACE_PERIOD_SECONDS='31'
+    else
+        TERMINATION_GRACE_PERIOD_SECONDS='30'
+    fi
+    patch_string="{\"spec\":{\"template\":{\"spec\":{\"terminationGracePeriodSeconds\":$TERMINATION_GRACE_PERIOD_SECONDS}}}}"
+    sudo kubectl -n $NS patch deployment $deployment_name -p $patch_string
+done
+echo
+}
+
+refresh-all-pods $NAMESPACE
